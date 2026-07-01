19143fbf54feedbdcc6e4a7f005c0b3341d0cf9d
Mvenkata-naga-sai <mahamkai.sai341@gmail.com>
2026-07-01 17:10:23 +0530

    Add files via upload

---
+++ models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.conf import settings
+from apps.posts.models import Post
+
+class Like(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
